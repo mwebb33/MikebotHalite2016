adaf0e81d665dab48be6cd9dc4cda272df7636ed
Michael Webb <mikewebb@wireless-10-146-226-202.public.utexas.edu>
2017-01-01 17:54:00 -0600

    Expands, moves inner to the right if > 30

--- MyBot.js
+++ MyBot.js
@@ -2,50 +2,81 @@ const {
   Move,
 } = require('./hlt');
 const Networking = require('./networking');
-const _ = require('lodash');
-const Logger = require('./Logger.js');
+const _ = require('./lodash.js');
+// const Logger = require('./Logger.js');
 
 const network = new Networking('MyJavaScriptBot');
 
+function checkExpandMove(currentSite, moveCandidates) {
+  var sortedCandidates = _.chain(moveCandidates)
+              .sortBy('strength')
+              .value();
+
+  var weakestCandidate = _.find(sortedCandidates, (candidate) => {
+    return currentSite.strength >= candidate.strength && currentSite.owner !== candidate.owner;
+  })
+
+  // Logger.info('weakestCandidate: ', weakestCandidate);
+  // Logger.info('currentStrength: ', currentSite.strength);
+
+  return weakestCandidate; 
+}
+
+function noOffensiveMoves(currentSite, moveCandidates) {
+  // Logger.info('canidates', moveCandidates);
+
+  return _.every(moveCandidates, ['owner', currentSite.owner]);
+}
+
 network.on('map', (gameMap, id) => {
   const moves = [];
-  const moveHash = {}; 
+  let moveHash = {}; 
 
   for (let y = 0; y < gameMap.height; y++) {
     for (let x = 0; x < gameMap.width; x++) {
       const loc = { x, y };
       const currentSite = gameMap.getSite(loc);
       if (currentSite.owner === id) {
-        let sites = []
+        let moveCandidates = []
 
-        sites.push(_.assign(gameMap.getSite(loc), {dir: 0, loc: gameMap.getLocation(loc, 0)})); 
-        sites.push(_.assign(gameMap.getSite(loc, 1), {dir: 1, loc: gameMap.getLocation(loc, 1)}));
-        sites.push(_.assign(gameMap.getSite(loc, 2), {dir: 2, loc: gameMap.getLocation(loc, 2)}));
-        sites.push(_.assign(gameMap.getSite(loc, 3), {dir: 3, loc: gameMap.getLocation(loc, 3)}));
-        sites.push(_.assign(gameMap.getSite(loc, 4), {dir: 4, loc: gameMap.getLocation(loc, 4)}));
+        moveCandidates.push(_.assign(gameMap.getSite(loc, 1), {dir: 1, loc: gameMap.getLocation(loc, 1)}));
+        moveCandidates.push(_.assign(gameMap.getSite(loc, 2), {dir: 2, loc: gameMap.getLocation(loc, 2)}));
+        moveCandidates.push(_.assign(gameMap.getSite(loc, 3), {dir: 3, loc: gameMap.getLocation(loc, 3)}));
+        moveCandidates.push(_.assign(gameMap.getSite(loc, 4), {dir: 4, loc: gameMap.getLocation(loc, 4)}));
 
-        var sortedSites = _.chain(sites)
-                    .sortBy('strength')
-                    .value();
+        // Check if move exists to expand or wait
+        let expandMove = checkExpandMove(currentSite, moveCandidates); 
 
-        var weakestSite = _.find(sortedSites, (candidate) => {
-          return currentSite.strength >= candidate.strength;
-        })
-
-        // Check if should expand
-        if (weakestSite.owner !== id) {
-          Logger.info('expand', currentSite.strength, weakestSite.strength);
-
-          let siteToMoveTo = gameMap.getLocation(loc, weakestSite.dir);
-          let moveString = weakestSite.loc.x + ' ' + weakestSite.loc.y
-          if (!!_.has(moveHash, moveString)) {
-            Logger.info('expand', currentSite.strength, weakestSite.strength);
-          } else {
-            Logger.info('duplicate', currentSite.strength, weakestSite.strength);
+        // Check if Expand
+        if (expandMove){
+          // Logger.info('expandMove: ', expandMove)
+          let moveSite = gameMap.getLocation(expandMove.loc, expandMove.dir);
+          let moveString = moveSite.x + ' ' + moveSite.y;
+          // Logger.info('expandMove moveString ', !_.has(moveHash, moveString))
+          if (!_.has(moveHash, moveString)) {
+            // Logger.info('expandMove Has Not Been Here: ', expandMove)
+            moveHash[moveString] = true;
+            moves.push(new Move(loc, expandMove.dir));
           }
+        // Random if no offensive moves
+        } else if ((noOffensiveMoves(currentSite, moveCandidates))){
+          // Logger.info('noOffensiveMoves')
+          let moveSite = gameMap.getLocation(loc, 2);
+          let moveString = moveSite.x + ' ' + moveSite.y;
+          if (!_.has(moveHash, moveString) && currentSite.strength > 40 ) {
+            // Logger.info('noOffensiveMoves Has Not Been Here: ', expandMove)
+            moveHash[moveString] = true;
+            moves.push(new Move(loc, 2));
+          } else if (currentSite.strength > 40 ) {
+            // Logger.info('noOffensiveMoves Has Not Been Here: ', expandMove)
+            moveHash[moveString] = true;
+            moves.push(new Move(loc, 1));          
+          }
+        } else {
 
-          moves.push(new Move(loc, weakestSite.dir));
         }
+
+        moveHash = {};
       }
     }
   }
